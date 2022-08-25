931c7847f8252ad37361bb4878eb8ab9b0e91d66
fefitti <fefitti@gmail.com>
2022-08-24 22:20:29 -0300

    ufa

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,10 @@
-Nome: Fernanda Fittipaldi Santos 
-Email: fefitti@gmail.com 
-Profissao: Quality assurence 
-Idade: 40 anos 
-Local: Ilhas Maldivas
+let Nome = 'Fernanda' 
+let Email = 'fefitti@gmail.com'
+let Profissao = 'QA'
+let Idade = '40'  
+let Local = 'Maldivas'
+
+
+let mensagem = "Olá, " + Nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + Local + " . Confirma pra gente alguns dados? A sua idade é " + Idade + ",  você é " + Profissao + " e o seu email é " + Email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log(mensagem)
