d97c6425cdf724798a585e457260a2ac4c3f100c
fefitti <fefitti@gmail.com>
2022-08-25 20:53:31 -0300

    feito

---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,3 @@
+let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
+let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
