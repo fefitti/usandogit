12eedb99612a7fc7c187abce02d19300ff0fad7b
fefitti <fefitti@gmail.com>
2022-08-27 17:40:14 -0300

    que sufoco

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,14 @@
+let multiplicador = 9
+let n = 9
+let i = 1
+
+while (i <= n) {
+    let valor = multiplicador * i
+    let texto = `${multiplicador} x ${i} = ${valor}`
+    console.log (texto)
+    i++    
+}
+
+    
+    
+
